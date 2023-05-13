031ad4b913c1107e096c6f2cf2cc7e5f5466ee70
swati <swati.bakshani@gmail.com>
2023-05-13 11:50:44 +0530

    new query added for boss

--- 29.sql
+++ 29.sql
@@ -9,6 +9,12 @@ Insert Into Employee_1 Values(102,'Maria',103);
 Insert Into Employee_1 Values(103,'Tom',NULL);
 Insert Into Employee_1 Values(104, 'David',103);
 
+-- answer 1
 select Emp.employeename, Mng.employeename As manager_name from employee_1 as Emp
 inner join employee_1 as Mng
 ON emp.managerID = Mng.employeeID;
+
+-- answer 2
+select emp.employeename, ISNULL (Mng.employeename, 'boss') as manager_name
+from employee_1 as emp left outer join employee_1 as mng
+on emp.managerid = mng.employeeid;
